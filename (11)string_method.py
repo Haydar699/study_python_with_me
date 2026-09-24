f123bb1e7a8082cf5d9a18aaf3805e8186627717
Haydar_fatah <muhammad.haydar.fatahillah@gmail.com>
2026-09-24 11:35:14 +0700

    add somethings

---
+++ (11)string_method.py
@@ -0,0 +1,18 @@
+### String Methods
+
+name =  "haydar fatah"
+phone_num  = 123
+
+# result = len(name)
+# result = name.find('a')
+# result = name.rfind("a")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.isalpha() # must be continued
+# name = name.isdigit()
+# name = name.count("a")
+name = name.replace("haydar", "Muh")
+
+print(name)
+
+### Exercise to validate a username
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,1 @@
-weight = float(input('enter ur weight: '))
-# unit = input("Kilogram or Pounds (K or L)? ").upper()
-
-# if unit == 'K':
-#     weight = weight * 2.205
-#     unit = 'lbs.'
-#     print(f"ur weight is {round(weight, 2)}{unit} ")
-# elif unit == 'L':
-#     weight = weight / 2.205
-#     unit = 'Kgs.'
-#     print(f"ur weight is {round(weight, 2)}{unit} ")
-# else:
-#     print('ur input is invalid!')
+123
