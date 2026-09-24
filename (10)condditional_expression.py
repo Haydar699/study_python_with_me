c600e644ca4f265dc78d4e544d56f71923ad062e
Haydar_fatah <muhammad.haydar.fatahillah@gmail.com>
2026-09-24 10:31:35 +0700

    add new lesson (conditional expression)

---
+++ (10)condditional_expression.py
@@ -0,0 +1,17 @@
+### conditional expression = one line shortcut for the if-else statement
+#                          = X if statement else Y
+
+num = 8
+num2 = 9
+age = 28
+temp = 28
+
+result1 = "Possitive" if num > 0 else "Negative"
+result2 = "EVEN" if num2 % 2 == 0 else "ADD"
+max_num = num if num > num2 else num2
+min_num = num if num < num2 else num2 
+status = "adult" if age >= 18 else "child"
+weather = "hot" if temp > 27 else "cold"
+
+
+print(weather)
